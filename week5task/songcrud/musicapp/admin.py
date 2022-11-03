79e991605a2419def59e0e0b174a6e97755df13d
timmyades3 <103801534+timmyades3@users.noreply.github.com>
2022-11-03 23:06:21 +0100

    Delete week5task directory

--- week5task/songcrud/musicapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Artiste, Lyric, Song
-
-# Register your models here.
-
-admin.site.register(Artiste)
-admin.site.register(Song)
-admin.site.register(Lyric)
